a8ddc77bfca6e38469eee32849fe33acf23e1cd4
Flexflext <felix.hereth.fh@googlemail.com>
2022-03-22 12:37:08 +0100

    ChainIK + TestRobot

--- MajorProject/Assets/Scripts/ChainIK.cs
+++ MajorProject/Assets/Scripts/ChainIK.cs
@@ -5,6 +5,9 @@ using UnityEditor;
 
 public class ChainIK : MonoBehaviour
 {
+    public bool DebugGizmos = true;
+    [Space]
+
     [Header("Chain Paramters")]
     [Tooltip("The Lenght of the Chain")]
     [Min(2.0f)]
@@ -24,6 +27,12 @@ public class ChainIK : MonoBehaviour
     [Min(0.001f)]
     [SerializeField] private float errorMargain = 0.01f;
 
+    [Tooltip("Snapback to the Start Position")]
+    [Range(0f, 1f)]
+    [SerializeField] private float snapbackStrenght = 1.0f;
+
+    #region//--> Position <--\\
+
     //All Bones
     private Transform[] bones;
 
@@ -36,10 +45,53 @@ public class ChainIK : MonoBehaviour
     //Lenght of all the Bone-Lenghts Combined
     private float completeLenght;
 
+    //Direction to from Root Bone to Target
+    private Vector3 rootTargetDirection;
+
+    #endregion
+
+    #region//--> Rotation <--\\
+
+    //Direction from bone to the Succesor Bone
     private Vector3[] startdirectionsSucc;
+
+    //StartRotation of Each Bone
     private Quaternion[] startRotationBone;
+
+    //Startrotation of the Target
     private Quaternion startRotationTarget;
+
+    //StartRotation of the Root Bone
     private Quaternion startRotationRoot;
+
+    #endregion
+
+    #region//--> Hint <--\\
+
+    //Plane to Project the Hint on to get the closest Rotation Angle
+    private Plane projectionPlane;
+
+    //Projected Position of the Hint on the projectionPlane
+    private Vector3 hintProjectionOnPlane;
+
+    //Projected Position of the bone on the projectionPlane
+    private Vector3 boneProjectedOnPlane;
+
+    //Angle on wich the Projected Position is the Closest to the Projected Hint
+    private float closestProjectionAngle;
+
+    #endregion
+
+    #region//--> Snapback <--\\
+
+    //Rotation of the Parent Object of the Root
+    private Quaternion rootRot;
+
+    //Rotation Difference of Root Object and Parent of Root
+    private Quaternion rootRotDiff;
+
+    #endregion
+
 
     private void Awake()
     {
@@ -51,8 +103,12 @@ public class ChainIK : MonoBehaviour
         ResolveIK();
     }
 
+    /// <summary>
+    /// Initialize the Chain Variables for IK System
+    /// </summary>
     private void InitializeChain()
     {
+        //Initialize the Array Sizes and Initial Variables
         bones  = new Transform[chainLength + 1];
         currentPositions = new Vector3[chainLength + 1];
         boneLenghts = new float[chainLength];
@@ -70,6 +126,7 @@ public class ChainIK : MonoBehaviour
 
             if (i == bones.Length - 1)
             {
+                //last Bone
                 startdirectionsSucc[i] = target.position - currentBone.position;
             }
             else
@@ -86,30 +143,99 @@ public class ChainIK : MonoBehaviour
         startRotationRoot = bones[0].rotation;
     }
 
+    /// <summary>
+    /// Resolve the IK System with Position and Rotation
+    /// </summary>
     private void ResolveIK()
     {
+        //Flags to Check if Chain is Okay and if Target os Filled
         if (target == null) return;
         if (boneLenghts.Length != chainLength) InitializeChain();
 
         //Get the Current Position
         for (int i = 0; i < bones.Length; i++) currentPositions[i] = bones[i].position;
 
-        Quaternion rootRot = (bones[0].parent != null) ? bones[0].parent.rotation : Quaternion.identity;
-        Quaternion rootRotDiff = rootRot * Quaternion.Inverse(startRotationRoot);
+        FabricAlgortihm();
+
+        if (hint != null)
+        {
+            AddHintOffset(ref currentPositions);
+        }
+
+        //Apply the Current Position and Rotation
+        for (int i = 0; i < currentPositions.Length; i++)
+        {
+            if (i == currentPositions.Length - 1)
+            {
+                //Multiplies the the target rotation with the Inverse of the startrotation of the target // --> get the Difference of the Rotation to the Start with Multiply of the current and the Original Rotation
+                bones[i].rotation = target.rotation * Quaternion.Inverse(startRotationTarget) * startRotationBone[i];
+            }
+            else
+            {
+                //Word Rotation from the Original Direction of the Bone to the Direction to the next Bone + Multiplies with start direction to get the Local Rotation
+                bones[i].rotation = Quaternion.FromToRotation(startdirectionsSucc[i], currentPositions[i + 1] - currentPositions[i]) * startRotationBone[i];
+            }
+
+            //Set Transform Position
+            bones[i].position = currentPositions[i];
+        }
+
+        
+    }
+
+    /// <summary>
+    /// Add the Hint Offset to the current
+    /// </summary>
+    private void AddHintOffset(ref Vector3[] _currentposarray)
+    {
+        for (int i = 1; i < _currentposarray.Length - 1; i++)
+        {
+            //Create Plane at Previous Position with Direction from Previous to CUrrent Position as the Normal
+            projectionPlane = new Plane(_currentposarray[i + 1] - _currentposarray[i - 1], _currentposarray[i - 1]);
+
+            //Project the Hint on the Created Plane
+            hintProjectionOnPlane = projectionPlane.ClosestPointOnPlane(hint.position);
+
+            //Project the current Bone on the Plane
+            boneProjectedOnPlane = projectionPlane.ClosestPointOnPlane(_currentposarray[i]);
+
+            //Calculate the Angle from the current Position ans the Hint from the Previous Position
+            closestProjectionAngle = Vector3.SignedAngle(boneProjectedOnPlane - _currentposarray[i - 1], hintProjectionOnPlane - _currentposarray[i - 1], projectionPlane.normal);
+
+            //Calculate the Position for the Current Position from the Angle Between + the previous Position and the Direction to from the Previous Position
+            _currentposarray[i] = Quaternion.AngleAxis(closestProjectionAngle, projectionPlane.normal) * (_currentposarray[i] - _currentposarray[i - 1]) + _currentposarray[i - 1];
+        }
+    }
+
+    /// <summary>
+    /// The F.A.B.R.I.K Algorthim (Forward And Backwords Reaching Inverse Kinematics)
+    /// </summary>
+    private void FabricAlgortihm()
+    {
+        //Rotation of the Parent of the Root -> Identity when null
+        rootRot = (bones[0].parent != null) ? bones[0].parent.rotation : Quaternion.identity;
+        //Root Rotation Difference between the Root Parent Rotation and the Start Root Rotation
+        rootRotDiff = rootRot * Quaternion.Inverse(startRotationRoot);
 
         //IK Calculations
         //Check for Target Outside of Chain Reach
         if ((target.position - bones[0].position).sqrMagnitude >= completeLenght * completeLenght)
         {
-            Vector3 direction = (target.position - currentPositions[0]).normalized;
+            rootTargetDirection = (target.position - currentPositions[0]).normalized;
 
             for (int i = 1; i < currentPositions.Length; i++)
             {
-                currentPositions[i] = currentPositions[i - 1] + direction * boneLenghts[i - 1];
+                currentPositions[i] = currentPositions[i - 1] + rootTargetDirection * boneLenghts[i - 1];
             }
         }
         else
         {
+            //Snapback to the Previous Position //-> More Dynamic Realistic Movement
+            for (int i = 0; i < currentPositions.Length - 1; i++)
+            {
+                currentPositions[i + 1] = Vector3.Lerp(currentPositions[i + 1], currentPositions[i] + rootRotDiff * startdirectionsSucc[i], snapbackStrenght);
+            }
+
             //Check Iterations
             for (int iterations = 0; iterations < maxIterations; iterations++)
             {
@@ -135,48 +261,22 @@ public class ChainIK : MonoBehaviour
                 if ((currentPositions[currentPositions.Length - 1] - target.position).sqrMagnitude <= errorMargain * errorMargain) break;
             }
         }
-
-        if (hint != null)
-        {
-            for (int i = 1; i < currentPositions.Length - 1; i++)
-            {
-                Plane plane = new Plane(currentPositions[i + 1] - currentPositions[i - 1], currentPositions[i - 1]);
-                Vector3 projectedHint = plane.ClosestPointOnPlane(hint.position);
-                Vector3 projectedBone = plane.ClosestPointOnPlane(currentPositions[i]);
-                float angle = Vector3.SignedAngle(projectedBone - currentPositions[i - 1], projectedHint - currentPositions[i - 1], plane.normal);
-                currentPositions[i] = Quaternion.AngleAxis(angle, plane.normal) * (currentPositions[i] - currentPositions[i - 1]) + currentPositions[i - 1];
-            }
-        }
-
-        //Apply the Current Position and Rotation
-        for (int i = 0; i < currentPositions.Length; i++)
-        {
-            if (i == currentPositions.Length - 1)
-            {
-                bones[i].rotation = target.rotation * Quaternion.Inverse(startRotationTarget) * startRotationBone[i];
-            }
-            else
-            {
-                bones[i].rotation = Quaternion.FromToRotation(startdirectionsSucc[i], currentPositions[i + 1] - currentPositions[i]) * startRotationBone[i];
-            }
-
-            bones[i].position = currentPositions[i];
-        }
-
-        
     }
 
 
     private void OnDrawGizmos()
     {
-        Transform current = this.transform;
-        for (int i = 0; i < chainLength && current != null && current.parent != null; i++)
+        if (DebugGizmos)
         {
-            float scale = Vector3.Distance(current.position, current.parent.position) * 0.1f;
-            Handles.matrix = Matrix4x4.TRS(current.position, Quaternion.FromToRotation(Vector3.up, current.parent.position - current.position), new Vector3(scale, Vector3.Distance(current.parent.position, current.position), scale));
-            Handles.color = Color.blue;
-            Handles.DrawWireCube(Vector3.up * 0.5f, Vector3.one);
-            current = current.parent;
+            Transform current = this.transform;
+            for (int i = 0; i < chainLength && current != null && current.parent != null; i++)
+            {
+                float scale = Vector3.Distance(current.position, current.parent.position) * 0.1f;
+                Handles.matrix = Matrix4x4.TRS(current.position, Quaternion.FromToRotation(Vector3.up, current.parent.position - current.position), new Vector3(scale, Vector3.Distance(current.parent.position, current.position), scale));
+                Handles.color = Color.blue;
+                Handles.DrawWireCube(Vector3.up * 0.5f, Vector3.one);
+                current = current.parent;
+            }
         }
     }
 }
